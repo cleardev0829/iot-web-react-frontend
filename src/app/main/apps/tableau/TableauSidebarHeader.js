69d5864f42da6aee1fbaa5cf5cce46489b5ad1ea
Win Nebergall <anatolloflint@gmail.com>
2022-03-03 13:36:02 +0800

    no message

--- src/app/main/apps/tableau/TableauSidebarHeader.js
+++ src/app/main/apps/tableau/TableauSidebarHeader.js
@@ -6,6 +6,7 @@ import { useDispatch, useSelector } from "react-redux";
 
 function TableauSidebarHeader() {
   const selected = useSelector(({ tableauApp }) => tableauApp.folders.selected);
+  const prefix = useSelector(({ tableauApp }) => tableauApp.folders.prefix);
 
   return (
     <div className="flex flex-col justify-center h-full p-24">
@@ -21,7 +22,7 @@ function TableauSidebarHeader() {
       </div>
       <FuseAnimate animation="transition.slideUpIn" delay={300}>
         <Typography className="text-16 sm:text-20 truncate">
-          {selected}
+          {selected && selected.substr(prefix.length + 1)}
         </Typography>
       </FuseAnimate>
     </div>
